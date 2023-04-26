b794afc73712f4b93acc4a119fe8354ac164d291
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-04-26 09:09:19 +0200

    more build files

---
+++ build.gradle.kts
@@ -0,0 +1,110 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val jvmTarget = "17"
+val micronautVersion = "3.8.8"
+val micrometerRegistryPrometheusVersion = "1.9.1"
+val junitJupiterVersion = "5.9.2"
+val logbackClassicVersion = "1.4.6"
+val logbackEncoderVersion = "7.3"
+val tcVersion = "1.17.6"
+val mockkVersion = "1.13.4"
+val kotestVersion = "5.5.5"
+
+
+group = "no.nav.hm"
+version = properties["version"] ?: "local-build"
+
+plugins {
+    kotlin("jvm") version "1.7.0"
+    kotlin("kapt") version "1.7.0"
+    id("java")
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+    id("io.micronaut.application") version "3.7.5"
+}
+
+configurations.all {
+    resolutionStrategy {
+        failOnChangingVersions()
+    }
+}
+
+dependencies {
+
+    api("ch.qos.logback:logback-classic:$logbackClassicVersion")
+    api("net.logstash.logback:logstash-logback-encoder:$logbackEncoderVersion")
+    // coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive")
+
+
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("io.micronaut:micronaut-runtime")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut:micronaut-http-server-netty")
+    implementation("io.micronaut:micronaut-http-client")
+    implementation("io.micronaut.cache:micronaut-cache-caffeine")
+    implementation("io.micronaut.micrometer:micronaut-micrometer-core")
+    implementation("io.micronaut.micrometer:micronaut-micrometer-registry-prometheus")
+    implementation("io.micronaut:micronaut-management")
+
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("io.micronaut.test:micronaut-test-kotest5")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:$kotestVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
+}
+
+micronaut {
+    version.set(micronautVersion)
+    testRuntime("netty")
+    testRuntime("junit5")
+    processing {
+        incremental(true)
+    }
+}
+
+application {
+    mainClass.set("no.nav.hm.grunndata.media.Application")
+}
+
+java {
+    sourceCompatibility = JavaVersion.toVersion(jvmTarget)
+    targetCompatibility = JavaVersion.toVersion(jvmTarget)
+    withSourcesJar()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = jvmTarget
+    kapt.includeCompileClasspath = false
+}
+
+tasks.named<KotlinCompile>("compileTestKotlin") {
+    kotlinOptions.jvmTarget = jvmTarget
+    kapt.includeCompileClasspath = false
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging {
+        events("skipped", "failed")
+        showExceptions = true
+        showStackTraces = true
+        showCauses = true
+        exceptionFormat = TestExceptionFormat.FULL
+        showStandardStreams = true
+    }
+}
+
+tasks.withType<Wrapper> {
+    gradleVersion = "8.0.1"
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://github-package-registry-mirror.gc.nav.no/cached/maven-release")
+    maven("https://packages.confluent.io/maven/")
+
+}
+
