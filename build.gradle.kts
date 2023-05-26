28b4f732e1462f015aaf5b5c2b36d9d2f44b3fc2
Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-05-26 09:21:24 +0200

    upgrade micronaut 3.9.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val jvmTarget = "17"
-val micronautVersion = "3.9.0"
+val micronautVersion = "3.9.2"
 val micrometerRegistryPrometheusVersion = "1.9.1"
 val junitJupiterVersion = "5.9.2"
 val logbackClassicVersion = "1.4.6"
@@ -21,7 +21,7 @@ plugins {
     kotlin("kapt") version "1.7.0"
     id("java")
     id("com.github.johnrengelman.shadow") version "7.1.0"
-    id("io.micronaut.application") version "3.7.8"
+    id("io.micronaut.application") version "3.7.9"
 }
 
 configurations.all {
