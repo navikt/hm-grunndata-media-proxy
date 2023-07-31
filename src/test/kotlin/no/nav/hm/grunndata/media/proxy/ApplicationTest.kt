c5b55d819274b48ba15dbd64701bedc7424c5b86
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-07-31 14:06:17 +0200

    upgrade micronaut 4.0.1

---
+++ src/test/kotlin/no/nav/hm/grunndata/media/proxy/ApplicationTest.kt
@@ -0,0 +1,13 @@
+package no.nav.hm.grunndata.media.proxy
+
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Test
+
+@MicronautTest
+class ApplicationTest {
+
+    @Test
+    fun runApplication() {
+        println("Application starts")
+    }
+}
