450ac13920669fa26bd753f26b593789fb355b45
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2023-11-14 09:55:34 +0100

    changed name to finnhjelpemiddel

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val jvmTarget = "17"
-val micronautVersion = "4.1.4"
+val micronautVersion = "4.1.6"
 val micrometerRegistryPrometheusVersion = "1.9.1"
 val junitJupiterVersion = "5.9.2"
 val logbackClassicVersion = "1.4.7"
