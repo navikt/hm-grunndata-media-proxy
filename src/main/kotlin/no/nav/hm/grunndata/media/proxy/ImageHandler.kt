ddfde7a912cf4908e5086396dfb96ab2d19f9d55
Dinh Anh Tuan Tran <dinh.anh.tuan.tran@nav.no>
2025-03-07 13:02:03 +0100

    support for metadata

--- src/main/kotlin/no/nav/hm/grunndata/media/proxy/ImageHandler.kt
+++ src/main/kotlin/no/nav/hm/grunndata/media/proxy/ImageHandler.kt
@@ -11,6 +11,7 @@ import java.io.InputStream
 import java.io.OutputStream
 import java.net.URI
 import javax.imageio.*
+import javax.imageio.metadata.IIOMetadata
 import kotlin.math.min
 
 
@@ -29,7 +30,8 @@ open class ImageHandler {
             LOG.info("reader: " + readers.next())
         }
     }
-    private fun resizeImage(image: BufferedImage, boundary: Dimension): BufferedImage {
+    private fun resizeImage(imageMeta: BufferedImageMetaData, boundary: Dimension): BufferedImageMetaData {
+        val image = imageMeta.bufferedImage
         val imageDimension = Dimension(image.width, image.height)
         val scaled = getScaledDimension(imageDimension, boundary)
         val resizedImage = BufferedImage(scaled.width, scaled.height, BufferedImage.TYPE_INT_RGB)
@@ -37,14 +39,15 @@ open class ImageHandler {
         graphics2D.drawImage(image, 0, 0, scaled.width, scaled.height, null)
         graphics2D.dispose()
         image.flush()
-        return resizedImage
+        return BufferedImageMetaData(resizedImage, imageMeta.metadata)
     }
 
-    private fun resizeImage(imageUri: URI, boundary: Dimension): BufferedImage {
+    private fun resizeImage(imageUri: URI, boundary: Dimension): BufferedImageMetaData {
         imageUri.toURL().openStream().use {
-            val image = readImage(it)
+            val imageMeta = readImage(it)
+            val image = imageMeta.bufferedImage
             return if (image.width > boundary.width || image.height > boundary.height)
-                resizeImage(image, boundary) else image
+                resizeImage(imageMeta, boundary) else imageMeta
         }
     }
 
@@ -58,9 +61,10 @@ open class ImageHandler {
 
     fun createImageVersion(sourceUri: URI, format: ImageFormat, imageVersion: Dimension): ByteArray {
         ByteArrayOutputStream().use {
-            val resized = resizeImage(sourceUri, imageVersion)
-            writeImage(resized, format.extension, it)
-            resized.flush()
+            val bufferedImageMetaData = resizeImage(sourceUri, imageVersion)
+            val image = bufferedImageMetaData.bufferedImage
+            writeImage(image, format.extension, it, bufferedImageMetaData.metadata)
+            image.flush()
             return it.toByteArray()
         }
     }
@@ -72,15 +76,16 @@ open class ImageHandler {
         return createImageVersion(sourceUri, format, imageVersion)
     }
 
-    private fun readImage(input: InputStream): BufferedImage {
+    private fun readImage(input: InputStream): BufferedImageMetaData {
         LOG.info("Reading image input stream witch cache setting: ${ImageIO.getUseCache()}")
         ImageIO.createImageInputStream(input).use {
             val readers = ImageIO.getImageReaders(it)
             val reader = readers.next()
             try {
                 reader.input = it
+                val metadata = reader.getImageMetadata(0)
                 val param = reader.defaultReadParam
-                return reader.read(0, param)
+                return BufferedImageMetaData(reader.read(0, param), metadata)
             }
             finally {
                 reader.dispose()
@@ -88,13 +93,13 @@ open class ImageHandler {
         }
     }
 
-    private fun writeImage(image: BufferedImage, format: String, output: OutputStream) {
+    private fun writeImage(image: BufferedImage, format: String, output: OutputStream, metadata: IIOMetadata) {
         ImageIO.createImageOutputStream(output).use {
             val writers = ImageIO.getImageWritersByFormatName(format)
             val writer = writers.next()
             try {
                 writer.output = it
-                writer.write(image)
+                writer.write(IIOImage(image, null, metadata))
             } finally {
                 writer.dispose()
             }
@@ -102,6 +107,7 @@ open class ImageHandler {
     }
 
 }
+data class BufferedImageMetaData(val bufferedImage: BufferedImage, val metadata: IIOMetadata)
 
 enum class ImageFormat(val extension: String) {
     JPG("jpg"),
